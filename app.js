5dc3b0ce99110b50afc77f5c67fdb9775eb66fd7
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-15 15:55:01 +0200

    SDI-NodeJS-10.1- SW API REST CRUD Canciones

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.set('crypto',crypto);
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app,gestorBD);
 require("./routes/rautores.js")(app, swig);
 require("./routes/rerrores.js")(app, swig);
 
