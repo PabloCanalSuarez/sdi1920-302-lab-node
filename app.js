53d0e7c8759a18117f96f2c0e1e92d3163cd8e44
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-15 16:03:28 +0200

    SDI-NodeJS-10.2- SW API REST Autenticación

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ let expressSession = require('express-session');
 let fs = require('fs');
 let https = require('https');
 
+var jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 app.use(expressSession({
     secret: 'abcdefg',
     resave: true,
@@ -18,6 +21,42 @@ app.use(expressSession({
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// routerUsuarioToken
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    var token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
+
 
 // routerUsuarioSession
 let routerUsuarioSession = express.Router();
